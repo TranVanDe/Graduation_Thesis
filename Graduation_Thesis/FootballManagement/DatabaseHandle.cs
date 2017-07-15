093224d37e4baadbb12ceeef0e68b4aad92899b3
TranVanDe <tranvande13@gmail.com>
2017-07-15 18:00:29 +0700

    Add files via upload

---
+++ Graduation_Thesis/FootballManagement/DatabaseHandle.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace FootballManagement
+{
+    class DatabaseHandle
+    {
+        SqlConnection conn = new SqlConnection(@"Data Source=DESKTOP-IBAC4RM\TVD;Initial Catalog=FootBallManagement;Integrated Security=True");
+        SqlCommand cmd = new SqlCommand();
+        SqlDataAdapter sqlAdap = new SqlDataAdapter();    
+
+        //Check Account
+        public bool CheckAccount(string username,string password)
+        {
+            DataTable dt = new DataTable();
+            conn.Open();
+
+            //Get data from Database
+            string query = "select username,password from [User]";
+            cmd.Connection = conn;
+            cmd.CommandText = query;
+            sqlAdap.SelectCommand = cmd;
+
+            //Fill data to DataTable
+            sqlAdap.Fill(dt);
+
+            //Check username and password
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                if (username.Equals(dt.Rows[i][0].ToString()) && password.Equals(dt.Rows[i][1].ToString()))
+                {
+                    conn.Close();
+                    return true;
+                }
+            }
+
+            conn.Close();
+            return false;
+        }
+    }
+}
---
+++ Graduation_Thesis/FootballManagement/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿using System.Drawing;
+
+namespace FootballManagement
+{
+    partial class frmLogIn
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLogIn));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lklCreate = new System.Windows.Forms.LinkLabel();
+            this.btnLogIn = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Buxton Sketch", 26F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(35, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(300, 43);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Football Management";
+            // 
+            // txtUser
+            // 
+            this.txtUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUser.Location = new System.Drawing.Point(129, 83);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(192, 32);
+            this.txtUser.TabIndex = 0;
+            // 
+            // txtPass
+            // 
+            this.txtPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPass.Location = new System.Drawing.Point(129, 134);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.PasswordChar = '*';
+            this.txtPass.Size = new System.Drawing.Size(192, 32);
+            this.txtPass.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(35, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Username :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(35, 144);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Password  :";
+            // 
+            // lklCreate
+            // 
+            this.lklCreate.AutoSize = true;
+            this.lklCreate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lklCreate.LinkColor = System.Drawing.Color.Cyan;
+            this.lklCreate.Location = new System.Drawing.Point(324, 235);
+            this.lklCreate.Name = "lklCreate";
+            this.lklCreate.Size = new System.Drawing.Size(46, 17);
+            this.lklCreate.TabIndex = 5;
+            this.lklCreate.TabStop = true;
+            this.lklCreate.Text = "Guest";
+            // 
+            // btnLogIn
+            // 
+            this.btnLogIn.BackColor = System.Drawing.Color.Aqua;
+            this.btnLogIn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnLogIn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLogIn.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnLogIn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Aqua;
+            this.btnLogIn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Lime;
+            this.btnLogIn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnLogIn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogIn.ForeColor = System.Drawing.Color.Red;
+            this.btnLogIn.Image = ((System.Drawing.Image)(resources.GetObject("btnLogIn.Image")));
+            this.btnLogIn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnLogIn.Location = new System.Drawing.Point(129, 185);
+            this.btnLogIn.Name = "btnLogIn";
+            this.btnLogIn.Size = new System.Drawing.Size(192, 37);
+            this.btnLogIn.TabIndex = 2;
+            this.btnLogIn.Text = "Sign In";
+            this.btnLogIn.UseVisualStyleBackColor = false;
+            this.btnLogIn.Click += new System.EventHandler(this.btnLogIn_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel1.LinkColor = System.Drawing.Color.Cyan;
+            this.linkLabel1.Location = new System.Drawing.Point(12, 235);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(58, 17);
+            this.linkLabel1.TabIndex = 6;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Sign Up";
+            // 
+            // frmLogIn
+            // 
+            this.AcceptButton = this.btnLogIn;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(382, 261);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.lklCreate);
+            this.Controls.Add(this.btnLogIn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmLogIn";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LOGIN";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnLogIn;
+        private System.Windows.Forms.LinkLabel lklCreate;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
