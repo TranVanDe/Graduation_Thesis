c57fc4799ff34a65a4fb6dd3567b2ff50c41df19
TranVanDe <tranvande13@gmail.com>
2017-07-16 08:03:36 +0700

    VER 0.1

---
+++ Graduation_Thesis _ver0.1/FootballManagement/frmMain.Designer.cs
@@ -0,0 +1,308 @@
+﻿namespace FootballManagement
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnChampion = new System.Windows.Forms.Button();
+            this.btnStatistic = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnSchedule = new System.Windows.Forms.Button();
+            this.btnTeam = new System.Windows.Forms.Button();
+            this.btnPlayer = new System.Windows.Forms.Button();
+            this.btnUser = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Buxton Sketch", 40F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(131, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(453, 67);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Football Management";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(72, 197);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Users";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(231, 197);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 24);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Players";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(406, 197);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 24);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Team";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(47, 367);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(105, 24);
+            this.label5.TabIndex = 9;
+            this.label5.Text = " Schedule";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(233, 367);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(76, 24);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Search";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(399, 367);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(80, 24);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Statistic";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.White;
+            this.label8.Location = new System.Drawing.Point(587, 367);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(45, 24);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Exit";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.White;
+            this.label9.Location = new System.Drawing.Point(556, 197);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(105, 24);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "Champion";
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.White;
+            this.btnExit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Aqua;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.Image = global::FootballManagement.Properties.Resources.exit;
+            this.btnExit.Location = new System.Drawing.Point(556, 261);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(105, 90);
+            this.btnExit.TabIndex = 7;
+            this.btnExit.UseVisualStyleBackColor = false;
+            // 
+            // btnChampion
+            // 
+            this.btnChampion.BackColor = System.Drawing.Color.White;
+            this.btnChampion.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnChampion.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Aqua;
+            this.btnChampion.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnChampion.Image = global::FootballManagement.Properties.Resources.football_chalice_shaped_trophy;
+            this.btnChampion.Location = new System.Drawing.Point(556, 91);
+            this.btnChampion.Name = "btnChampion";
+            this.btnChampion.Size = new System.Drawing.Size(105, 90);
+            this.btnChampion.TabIndex = 3;
+            this.btnChampion.UseVisualStyleBackColor = false;
+            // 
+            // btnStatistic
+            // 
+            this.btnStatistic.BackColor = System.Drawing.Color.White;
+            this.btnStatistic.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnStatistic.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Aqua;
+            this.btnStatistic.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStatistic.Image = global::FootballManagement.Properties.Resources.statistical_chart;
+            this.btnStatistic.Location = new System.Drawing.Point(388, 261);
+            this.btnStatistic.Name = "btnStatistic";
+            this.btnStatistic.Size = new System.Drawing.Size(105, 90);
+            this.btnStatistic.TabIndex = 6;
+            this.btnStatistic.UseVisualStyleBackColor = false;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.BackColor = System.Drawing.Color.White;
+            this.btnSearch.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSearch.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Aqua;
+            this.btnSearch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSearch.Image = global::FootballManagement.Properties.Resources.magnifier;
+            this.btnSearch.Location = new System.Drawing.Point(220, 261);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(105, 90);
+            this.btnSearch.TabIndex = 5;
+            this.btnSearch.UseVisualStyleBackColor = false;
+            // 
+            // btnSchedule
+            // 
+            this.btnSchedule.BackColor = System.Drawing.Color.White;
+            this.btnSchedule.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSchedule.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Aqua;
+            this.btnSchedule.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSchedule.Image = global::FootballManagement.Properties.Resources.calendar_with_a_clock_time_tools;
+            this.btnSchedule.Location = new System.Drawing.Point(51, 261);
+            this.btnSchedule.Name = "btnSchedule";
+            this.btnSchedule.Size = new System.Drawing.Size(105, 90);
+            this.btnSchedule.TabIndex = 4;
+            this.btnSchedule.UseVisualStyleBackColor = false;
+            // 
+            // btnTeam
+            // 
+            this.btnTeam.BackColor = System.Drawing.Color.White;
+            this.btnTeam.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnTeam.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Aqua;
+            this.btnTeam.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTeam.Image = global::FootballManagement.Properties.Resources.football_fans_group;
+            this.btnTeam.Location = new System.Drawing.Point(388, 91);
+            this.btnTeam.Name = "btnTeam";
+            this.btnTeam.Size = new System.Drawing.Size(105, 90);
+            this.btnTeam.TabIndex = 2;
+            this.btnTeam.UseVisualStyleBackColor = false;
+            // 
+            // btnPlayer
+            // 
+            this.btnPlayer.BackColor = System.Drawing.Color.White;
+            this.btnPlayer.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnPlayer.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Aqua;
+            this.btnPlayer.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPlayer.Image = global::FootballManagement.Properties.Resources.football_player_attempting_to_kick_ball;
+            this.btnPlayer.Location = new System.Drawing.Point(220, 91);
+            this.btnPlayer.Name = "btnPlayer";
+            this.btnPlayer.Size = new System.Drawing.Size(105, 90);
+            this.btnPlayer.TabIndex = 1;
+            this.btnPlayer.UseVisualStyleBackColor = false;
+            // 
+            // btnUser
+            // 
+            this.btnUser.BackColor = System.Drawing.Color.White;
+            this.btnUser.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnUser.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Aqua;
+            this.btnUser.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnUser.Image = global::FootballManagement.Properties.Resources.football_leader_man__1_;
+            this.btnUser.Location = new System.Drawing.Point(51, 91);
+            this.btnUser.Name = "btnUser";
+            this.btnUser.Size = new System.Drawing.Size(105, 90);
+            this.btnUser.TabIndex = 0;
+            this.btnUser.UseVisualStyleBackColor = false;
+            this.btnUser.Click += new System.EventHandler(this.btnUser_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(715, 437);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.btnChampion);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.btnStatistic);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnSchedule);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnTeam);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnPlayer);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnUser);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Football Management";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnUser;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnPlayer;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnTeam;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnSchedule;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnStatistic;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button btnChampion;
+    }
+}
